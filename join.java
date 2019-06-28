643335df7488365ad0a8a4b2d7b89e883e5ca6ba
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-06-28 15:41:45 +0530

    Create join.java

---
+++ join.java
@@ -0,0 +1,13 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class str {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.next();
+		String s = sc.next();
+	    System.out.println(s1.concat(s));
+		
+		}
+}
