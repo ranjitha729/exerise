6cedef0d4778c3ece2344cd82bb47dbee87d3f18
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-06-28 15:56:51 +0530

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,22 @@
+package guvi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class str {
+	public static void main(String[] args) {
+		int n;
+		Scanner	sc = new Scanner(System.in);		
+	    n = sc.nextInt();
+	    int s = sc.nextInt();
+	    int a[] = new int[n];
+	    for(int i=0;i<n;i++){
+		    a[i] = sc.nextInt();
+	}
+	   Arrays.sort(a); 
+	  System.out.println(a[s-1]);
+	
+		
+		
+		}
+}
