8ea95e0273e339ba08f49b07af2970f8a557145f
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-06-28 17:58:42 +0530

    Create arrmin.java

---
+++ arrmin.java
@@ -0,0 +1,18 @@
+package guvi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class str {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a[] = new int[n];
+		for(int i=0;i<n;i++){
+			a[i] = sc.nextInt();
+		}
+		Arrays.sort(a);
+		System.out.println(a[0]);
+		
+}
+}
