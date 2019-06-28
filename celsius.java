03ab15e54e43044de000d1cae85afd1d1289c05e
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-06-28 16:10:26 +0530

    Create celsius.java

---
+++ celsius.java
@@ -0,0 +1,17 @@
+package guvi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class str {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	    int c = sc.nextInt();
+	    int k =0;
+	    
+	  System.out.println(k = c +273);
+	
+		
+		
+		}
+}
