5224a5f3f9fd9b9c04b072ca392d07377711f515
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-06-29 11:33:30 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,18 @@
+package guvi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class str {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int t =0;
+		t = n1;
+		n1 = n2;
+		n2 = t;
+		System.out.println(n1 +" "+n2);
+		
+}
+}
