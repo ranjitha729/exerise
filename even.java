29621aa3dfd0a23264b0d84b62f5cd1417d636a7
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-07-01 14:07:14 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,20 @@
+package guvi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class str {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n % 2==0){
+        	System.out.println(n);
+        }
+        else{
+        	System.out.println(n-1);
+        }
+			
+			
+			
+}
+}
