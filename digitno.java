8108ee813f051203108e8f15a2791a640fcb07f1
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-07-01 13:50:28 +0530

    Rename digit.java to digitno.java

---
+++ digitno.java
@@ -0,0 +1,19 @@
+package guvi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class str {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = String.valueOf(n);
+        for(int i=0;i<s.length();i++){
+        System.out.print(s.charAt(i) +" ");
+        }
+				
+			
+			
+			
+}
+}
