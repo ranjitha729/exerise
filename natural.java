07d0b737d8d80f2abd1ea02ed8b3307fe3451c07
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-07-01 14:34:27 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,15 @@
+package guvi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class str {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();int c=0;
+		for(int i =1;i<=n;i++)	{
+			c = c + i;
+		}
+		System.out.println(c);
+}
+}
