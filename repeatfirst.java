b78d4c42562f9fd2710e68b9be0f95eb85eb7183
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-07-02 12:32:19 +0530

    Create repeatfirst.java

---
+++ repeatfirst.java
@@ -0,0 +1,41 @@
+package guvi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class str {
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a[] = new int[n];
+		int val=0;
+		for(int i=0;i<n;i++){
+			a[i] = sc.nextInt();
+		}
+		int count=0,k=0,min=n;
+		for(int i=0;i<n;i++){
+			count=0;
+			
+			for(int j=i+1;j<n;j++){
+				if(a[i]==a[j]){
+					val = a[i];
+					k=i;
+                    count++;
+				}
+                }
+			if(count>0){
+				if((min>k)){
+					min=k;
+				}
+				
+			}
+		}
+		System.out.println(a[min]);
+        
+		}
+		}
+		
+	
+	
+
+
