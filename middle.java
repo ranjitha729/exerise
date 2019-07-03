cda811f7835b53006c7565f304c320eb3cdfde3d
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-07-03 18:02:00 +0530

    Create middle.java

---
+++ middle.java
@@ -0,0 +1,20 @@
+package guvi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class str {
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a[] = new int [n];
+		for(int i=0;i<n;i++){
+			a[i] = sc.nextInt();
+		}
+			Arrays.sort(a);
+			System.out.println(a[n/2]);
+		}
+		
+}
