f8a2a531be56edfc8c9d9748237e6b1c15fb43d9
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-07-03 10:02:49 +0530

    Create days.java

---
+++ days.java
@@ -0,0 +1,19 @@
+package guvi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class str {
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		if(s.equals("sunday") || s.equals("saturday")){
+			System.out.println("yes");
+		}
+		else{
+			System.out.println("no");
+		}
+		}
+		}
