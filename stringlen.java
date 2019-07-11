eb04dd277f42103bbf1ca743fdd31a156a41a53a
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-07-11 18:43:13 +0530

    Create stringlen.java

---
+++ stringlen.java
@@ -0,0 +1,15 @@
+package guvi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class largeno {
+public static void main(String args[]){
+	Scanner sc = new Scanner(System.in);
+	String s = sc.next();int c=0;
+	for(int i=0;i<s.length();i++){
+		c++;
+	}
+	System.out.println(c);
+}
+}
