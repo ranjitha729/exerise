3a9d061a39874f58ee888dd2ab47cbc6d38e3717
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-07-15 17:16:21 +0530

    Create lastelement.java

---
+++ lastelement.java
@@ -0,0 +1,19 @@
+package guvi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class largeno {
+public static void main(String args[]){
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	int a [] = new int [n];
+	for(int i=0;i<n;i++){
+		a[i]=sc.nextInt();
+	}
+	Arrays.sort(a);
+		System.out.print(a[n-1]);
+		
+}
+}
+
