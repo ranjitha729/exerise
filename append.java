66806cf5b707eb9272ba6b97fc20e57908956247
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-07-15 17:11:43 +0530

    Create append.java

---
+++ append.java
@@ -0,0 +1,13 @@
+package guvi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class largeno {
+public static void main(String args[]){
+	Scanner sc = new Scanner(System.in);
+	String s = sc.nextLine();
+	StringBuffer str = new StringBuffer(s);
+	System.out.println(str.append(" "+"Answer"));
+}
+}
