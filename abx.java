b94165677bf012a85511281e21aa17107096a8c8
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-07-24 19:00:21 +0530

    Create abx.java

---
+++ abx.java
@@ -0,0 +1,15 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class reverse {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int x=sc.nextInt();
+		int y=a*x+b;
+		System.out.println(y);
+		
+}
+}
