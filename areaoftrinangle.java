81b7f29b16d11d0ef5716b92457a5d87405b0f0a
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-07-24 18:22:27 +0530

    Create areaoftrinangle.java

---
+++ areaoftrinangle.java
@@ -0,0 +1,14 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class reverse {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int h=sc.nextInt();
+		int b=sc.nextInt();
+		int a=h*b/2;
+		System.out.println(a);
+		
+}
+}
