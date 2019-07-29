3b0dc6f4afd8a7d00d4bd2be713ed55cbd4dfd8b
ranjitha729 <43001683+ranjitha729@users.noreply.github.com>
2019-07-29 14:58:52 +0530

    Create containsstring.java

---
+++ containsstring.java
@@ -0,0 +1,20 @@
+package exampleprogram;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class encoding {
+public static void main(String [] args){
+	    	   Scanner in = new Scanner (System.in);
+	    	   String s = in.next();
+	    	   String p = in.next();
+	    	   if(s.contains(p)){
+	    		   System.out.println("yes");
+	    	   }
+	    	   else{
+	    		   System.out.println("no");
+	    	   }
+	           
+	       }
+}
+	   
